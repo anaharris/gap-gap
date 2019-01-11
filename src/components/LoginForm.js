751442c938a8564d95846153c3be261dcd9c131c
Ana Harris <vana1990@gmail.com>
2019-01-11 15:13:35 -0500

    changed auth

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router'
+import { Button, Form, Segment, Message } from 'semantic-ui-react'
 
 class LoginForm extends Component {
+  state = {
+    username: '',
+    password: ''
+  }
+
+  handleChange = (e, {name, value}) => {
+    this.setState({ [name]: value })
+  }
+
+  handleLoginSubmit = () => {
+    console.log('fetching')
+  }
+
   render() {
     return (
-      <div>
-        login form
-       </div>
+      <Segment>
+        <Form
+          onSubmit={this.handleLoginSubmit}
+          size='mini'
+          key='mini'
+          loading={this.props.authenticatingUser}
+          error={this.props.failedLogin}
+        >
+          <Message
+            error
+            header={this.props.failedLogin ? this.props.error : null}
+          />
+          <Form.Group widths='equal'>
+            <Form.Input
+              label='username'
+              placeholder='username'
+              name='username'
+              onChange={this.handleChange}
+              value={this.state.username}
+            />
+            <Form.Input
+              type='password'
+              label='password'
+              placeholder='password'
+              name='password'
+              onChange={this.handleChange}
+              value={this.state.password}
+            />
+          </ Form.Group>
+          <Button type='submit'>Login</Button>
+        </ Form>
+      </Segment>
     )
   }
 
 }
 
 
-export default LoginForm
+export default withRouter(LoginForm)
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
 import Search from './Search'
+import { Menu } from 'semantic-ui-react'
 
 class Navbar extends Component {
+
   render() {
     return (
-      <div>Navbar
-        <Search />
-      </div>
+      <Menu color={'red'}>
+        <Menu.Item
+          name='home'
+          onClick={this.homeClick}/>
+        <Menu.Item
+          name='addBot'
+          onClick={this.addBotClick}/>
+      </Menu>
     )
   }
+
+  homeClick = () => {
+    console.log('clicking home')
+  }
+
+  addBotClick = () => {
+    console.log('clicking add bot')
+  }
+
 }
 
 export default Navbar
