03df1afeefba61d5675e101c1ab0ccb10b352b3d
Ana Harris <vana1990@gmail.com>
2019-01-13 18:51:41 -0500

    login with redirect

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import MessagesContainer from './MessagesContainer'
 import ConversationsContainer from './ConversationsContainer'
 import Navbar from './Navbar'
-
+import { Redirect } from 'react-router-dom'
+const Cookies = require('cookies-js')
 
 class Homepage extends Component {
   render(){
-    return(
+  return Cookies.get('token') ? (
       <div>
         <Navbar setUser={this.props.setUser}/>
         <ConversationsContainer />
         <MessagesContainer />
       </div>
-    )
+    ) : <Redirect to='/login' />
   }
 }
 
