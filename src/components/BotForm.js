b180bddc1d03ff45b7e9c004d6a913cbfed9e15c
Ana Harris <vana1990@gmail.com>
2019-01-22 17:02:33 -0500

    added local state to the bot form component

--- src/components/BotForm.js
+++ src/components/BotForm.js
@@ -4,6 +4,23 @@ import { connect } from 'react-redux'
 import { closeNewBotModal } from '../redux/actions.js'
 
 class BotForm extends Component {
+  state = {
+    name: '',
+    trigger: '',
+    response: ''
+  }
+
+  handleName = (e) => {
+    this.setState({name: e.target.value})
+  }
+
+  handleTrigger = (e) => {
+    this.setState({trigger: e.target.value})
+  }
+
+  handleResponse = (e) => {
+    this.setState({response: e.target.value})
+  }
 
   render() {
     return (
@@ -12,13 +29,20 @@ class BotForm extends Component {
         <Form>
           <Form.Input
             placeholder='Bot name'
+            onChange={this.handleName}
           />
           <Header>Trigger:</Header>
           <p>Trigger should be a single word.</p>
-          <Form.Input/>
+          <Form.Input
+            placeholder='Trigger'
+            onChange={this.handleTrigger}
+          />
           <Header>Response:</Header>
           <p>Response can be a word or a sentence.</p>
-          <Form.Input/>
+          <Form.Input
+            placeholder='Response'
+            onChange={this.handleResponse}
+          />
           <Button
             fluid
             size='small'
